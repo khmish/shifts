6c33fc4ce5fed7b49f4047ba8aa41f18c28c72fd
khmish <30586743+khmish@users.noreply.github.com>
2024-05-25 19:40:49 +0300

    add permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,12 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+    use HasRoles;
 
     public function canAccessPanel(Panel $panel): bool
     {
