cdd2cd6b5eeddc652043b0bce71aabde9d304969
khmish <30586743+khmish@users.noreply.github.com>
2024-05-25 20:53:22 +0300

    update

---
+++ app/Models/User copy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use OwenIt\Auditing\Contracts\Auditable;
+
+
+class User extends Authenticatable implements FilamentUser,Auditable
+{
+    use HasFactory, Notifiable ,\OwenIt\Auditing\Auditable;
+    use HasRoles;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+        // You may optionally implement your own logic here when a user has access to this panel.
+        // return $this->email === 'hk0021@gmail.com';
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
